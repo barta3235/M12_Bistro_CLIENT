7dea45069b546fac35178388fac220ab53f8a3a0
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-05-19 02:30:32 +0600

    parallex and testimonial

--- src/pages/home/featured/Featured.jsx
+++ src/pages/home/featured/Featured.jsx
@@ -3,9 +3,9 @@ import featuredImage from '../../../assets/home/featured.jpg'
 import './featured.css'
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle subHeading={"Check It Out"} heading={"Featured Item"}></SectionTitle>
-            <div className="md:flex items-center justify-center py-20 px-36">
+            <div className="md:flex items-center justify-center py-20 px-36 bg-slate-50 bg-opacity-40">
                 <div>
                     <img src={featuredImage} alt="" />
                 </div>
@@ -13,7 +13,7 @@ const Featured = () => {
                     <p>Aug 2029</p>
                     <p className="uppercase">Where can I get to eat</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis quidem illum fugiat officia. Tempore, illum. Cupiditate reprehenderit qui expedita quidem quam maiores quos quae nobis ipsum iusto dolore, impedit commodi suscipit iure at aperiam veritatis modi dolor. Sequi quas ea dignissimos nobis, voluptatum id voluptate reiciendis culpa beatae laudantium eum!</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 ">Order Now</button>
                 </div>
             </div>
         </div>
