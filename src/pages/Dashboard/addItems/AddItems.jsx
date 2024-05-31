c5fca9295828b870a11f28b2032b0d009953f6ca
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-05-31 18:20:56 +0600

    add item frontend

--- src/pages/Dashboard/addItems/AddItems.jsx
+++ src/pages/Dashboard/addItems/AddItems.jsx
@@ -1,13 +1,13 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/sectiontitle/SectionTitle";
 import { Helmet } from "react-helmet-async";
+import { FaUtensils } from "react-icons/fa";
 
 const AddItems = () => {
     const {
         register,
         handleSubmit,
         watch,
-
     } = useForm()
 
     const onSubmit = (data) => {
@@ -23,17 +23,59 @@ const AddItems = () => {
             </div>
             <div>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input placeholder="Recipe Name" className="border" {...register("name")} />
-                    <select  {...register("category")} className="select select-bordered w-full max-w-xs">
-                        <option disabled selected>Select a category</option>
-                        <option value="salad">Salad</option>
-                        <option value="pizza">Pizza</option>
-                        <option value="soup">Soup</option>
-                        <option value="dessert">Dessert</option>
-                        <option value="drinks">Drinks</option>
-                    </select>
 
-                    <input type="submit" />
+
+                    <div className="form-control w-full my-6">
+                        <label className="label">
+                            <span className="label-text">Recipe Name*</span>
+                        </label>
+                        <input {...register("name",{required:true})} type="text" placeholder="Recipe Name" className="input input-bordered w-full" />
+                    </div>
+
+                    <div className="flex justify-between gap-5 items-center">
+
+                        {/* category */}
+
+                        <div className="form-control w-full my-6">
+                            <label className="label">
+                                <span className="label-text">Category*</span>
+                            </label>
+                            <select  {...register("category",{required:true})} className="select select-bordered w-full">
+                                <option disabled selected>Select a category</option>
+                                <option value="salad">Salad</option>
+                                <option value="pizza">Pizza</option>
+                                <option value="soup">Soup</option>
+                                <option value="dessert">Dessert</option>
+                                <option value="drinks">Drinks</option>
+                            </select>
+
+                        </div>
+
+                        {/* price */}
+
+                        <div className="form-control w-full my-6">
+                            <label className="label">
+                                <span className="label-text">Price*</span>
+                            </label>
+                            <input {...register("price",{required:true})} type="number" placeholder="Price in dollars" className="input input-bordered w-full" />
+                        </div>
+
+                    </div>
+
+                    <div>
+                        <label className="form-control w-full my-6">
+                            <div className="label">
+                                <span className="label-text">Recipe Details</span>
+                            </div>
+                            <textarea {...register('recipe',{required:true})}  className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                        </label>
+                    </div>
+
+                    <div className="my-6">
+                        <input {...register('image',{required:true})} type="file" className="file-input w-full max-w-xs" />
+                    </div>
+
+                    <button className="btn flex justify-center items-center gap-2">Add Item <FaUtensils></FaUtensils></button>
                 </form>
             </div>
 
